ff429d90ffad6acd2608a220b8b6bdac25db6cfc
Korey Andrus <korey86@gmail.com>
2021-12-14 14:31:17 -0600

    Finish backend, work on styling

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const searchElement = document.querySelector(['data-city-search'])
+const searchElement = document.querySelector('[data-city-search]')
 const searchBox = new google.maps.places.SearchBox(searchElement);
 searchBox.addListener('places_changed', () => {
     const place = searchBox.getPlaces()[0];
@@ -17,6 +17,25 @@ searchBox.addListener('places_changed', () => {
             longitude: longitude
         })
     }).then(res => res.json()).then(data => {
-        setWeatherData(data, place.formatted_address)
+        setWeatherData(data, data.location.name)
     })
 })
+
+const icon = document.querySelector('.icon-container');
+const locationElement = document.querySelector('[data-location]')
+const statusElement = document.querySelector('[data-status]')
+const temperatureElement = document.querySelector('[data-temperature]')
+const precipitationElement = document.querySelector('[data-precipitation]')
+const windElement = document.querySelector('[data-wind]')
+
+
+
+function setWeatherData(data, place) {
+    console.log(data);
+    locationElement.textContent = place;
+    statusElement.textContent = data.current.condition.text;
+    temperatureElement.textContent = data.current.temp_f;
+    precipitationElement.textContent = `${data.current.precip_in}"`;
+    windElement.textContent = data.current.wind_mph;
+    icon.innerHTML = `<img src="${data.current.condition.icon}">`
+}
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
-const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const axios = require('axios');
 const express = require('express');
 const app = express();
 
@@ -10,8 +11,12 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.post('/weather', (req, res) => {
-    console.log(req.body);
-})
+    const url = `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${req.body.latitude},${req.body.longitude}&aqi=no`;
+    axios({
+        url: url,
+        responseType: 'json'
+      }).then(data => res.json(data.data))
+    })
 
 app.listen(3000, () => {
     console.log('Server Started');
